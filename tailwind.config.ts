1bbba7e12c1d7ea751f6fa0008aa39769f4bdae5
SAGARSAGU347 <162672930+SAGARSAGU347@users.noreply.github.com>
2025-01-12 02:45:47 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,17 +2,23 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        text_header_blue: "#6675F7",
+        ContactColorGradient1: "#FFC656",
+        ContactColorGradient2: "#F16063",
+        FooterColorGradient1: "rgba(170, 20, 240, 0.4)",
+        FooterColorGradient2: "rgba(255, 62, 84, 0.4)"
       },
     },
   },
   plugins: [],
 } satisfies Config;
+
